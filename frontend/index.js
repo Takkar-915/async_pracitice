6d15850873cf124c10f6341a635324e570159b5a
takkar <yurisuki915@gmail.com>
2022-11-10 08:43:23 +0900

    APIで取得したデータをテーブル表示

--- frontend/index.js
+++ frontend/index.js
@@ -1,37 +1,62 @@
 const getBooksInformation = async() => {
-    // 本の一覧の取得
-    const url = "http://localhost/PHP/front-back/backend/books.php";
-
-    const json = await fetch(url)   
-        .then(res => {
-            console.log("取得成功")
-            return res
-        }).catch(error => {
-            console.log("取得失敗")
-            return null
-    });
+    try{
+        const response = await fetch(url);
+        if (response.ok){
+            const body = await response.json()
+            const books = body.books
+            return books
+        }else{
+            throw new Error("リクエスト失敗")
+        }
+    }catch(error){
+        console.error(error)
+    }
 };
 
-const generate_table = ($booksInformation) => {
+const renderBooks = async (url) => {
+    const books = await getBooksInformation(url);
     let body = document.getElementsByTagName("body")[0];
 
     let tbl = document.createElement("table");
     let tblBody = document.createElement("tbody");
 
-    let len = Object.keys($booksInformation).length;
+    let rowLen = books.length;
+    let columnsLen = 4;
 
-    for (let i = 0; i < len; i++){
+    for (let i = 0; i < rowLen; i++){
         let row = document.createElement("tr");
 
-        for (let j = 0; j < len; j++){
+        for (let j = 0; j < columnsLen; j++){
             let cell = document.createElement("td");
-            let cellText = document.createTextNode("test");
-            cell.appendChild(cellText);
-            row.appendChild(cell);
+            if (j == 0){
+                let cellText = document.createTextNode(books[i].id);
+                cell.appendChild(cellText);
+                row.appendChild(cell);
+                tblBody.appendChild(row);
+            }else if (j == 1){
+                let cellText = document.createTextNode(books[i].book_name);
+                cell.appendChild(cellText);
+                row.appendChild(cell);
+                tblBody.appendChild(row);
+            }else if (j == 2){
+                let cellText = document.createTextNode(books[i].author_name);
+                cell.appendChild(cellText);
+                row.appendChild(cell);
+                tblBody.appendChild(row);
+            }else if (j == 3){
+                let cellText = document.createTextNode(books[i].date_of_issue);
+                cell.appendChild(cellText);
+                row.appendChild(cell);
+                tblBody.appendChild(row);
+            }
         }
-        tblBody.appendChild(row);
     }
     tbl.appendChild(tblBody);
     body.appendChild(tbl);
     tbl.setAttribute("border","2");
-};
+    
+}
+
+const url = "http://localhost/PHP/front-back/backend/books.php";
+
+renderBooks(url);
